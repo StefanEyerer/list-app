84cb9e4fe4e59a422a4bb8b00a30727a696dbe85
Stefan Eyerer <stefan.eyerer@gmail.com>
2022-03-10 16:55:00 +0100

    chore: setup database connection

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,19 @@
+import * as mongoose from 'mongoose';
 import app from './app';
 
 const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+const databaseUrl = process.env.NX_DATABASE_URL;
+
+startUp();
+
+async function startUp(): Promise<void> {
+  try {
+    await mongoose.connect(databaseUrl);
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}/api`);
+    });
+  } catch (error) {
+    console.error(`An error has occured: ${error}`);
+    process.exit(1);
+  }
+}
