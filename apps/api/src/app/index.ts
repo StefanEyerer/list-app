45323c17e3fa2c0372176da34519d1e87ddadf78
Stefan Eyerer <stefan.eyerer@gmail.com>
2022-05-25 19:55:00 +0200

    feat: add production middleware

--- apps/api/src/app/index.ts
+++ apps/api/src/app/index.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import * as compression from 'compression';
+import helmet from 'helmet';
 import apiRouter from './router';
 
 const app = express();
 
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.use(express.json());
 
 app.use('/api', apiRouter);
